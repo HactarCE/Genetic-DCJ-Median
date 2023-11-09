5884612e777c0136803ee054268bbed436dfac76
Aaron Dias Barreto <aaronadb@gmail.com>
2023-11-08 23:44:46 -0500

    Added Crossover and Mutation functions

---
+++ Crossover.jl
@@ -0,0 +1,28 @@
+include("Fitness.jl")
+include("DCJ.jl")
+include("Mutate.jl")
+function Crossover(Parent1::Vector{Int}, Parent2::Vector{Int}, G1::Vector{Int}, G2::Vector{Int}, G3::Vector{Int})
+    # Compute the fitness score for both the parents
+    f1=Fitness(G1,G2,G3,Parent1)
+    f2=Fitness(G1,G2,G3,Parent2)
+    # Create one child by applying a random number of DCJ operations to the parent with the worse fitness score
+    if(f1>f2)
+        Child1=DCJ(Parent2,Parent1,rand(1:length(Parent1)+1))
+        Child2=Parent1
+    else
+        Child1=DCJ(Parent1,Parent2,rand(1:length(Parent2)+1))
+        Child2=Parent2
+    end
+    # Mutate both the generated children
+    mut1=Mutate(Child1,G1,G2,G3)
+    mut2=Mutate(Child2,G1,G2,G3)
+    # Calculate the fitness scores of all the four genomes
+    fc1=Fitness(G1,G2,G3,Child1)
+    fc2=Fitness(G1,G2,G3,Child2)
+    fmut1=Fitness(G1,G2,G3,mut1)
+    fmut2=Fitness(G1,G2,G3,mut2)
+    scores=[[fc1,Child1],[fc2,Child2],[fmut1,mut1],[fmut2,mut2]]
+    sort!(scores)
+    # return the best 2 children based on the fitness scores
+    return scores[4][2], scores[3][2]
+end
---
+++ Mutate.jl
@@ -0,0 +1,25 @@
+include("DCJdistance.jl")
+function Mutate(Child::Vector{Int}, G1::Vector{Int}, G2::Vector{Int}, G3::Vector{Int})
+    # Estimate the distances to each of the genomes
+    d1_est=(DCJdistance(G1,G2)+DCJdistance(G1,G3)-DCJdistance(G2,G3))/2
+    d2_est=(DCJdistance(G1,G2)+DCJdistance(G2,G3)-DCJdistance(G1,G3))/2
+    d3_est=(DCJdistance(G1,G3)+DCJdistance(G2,G3)-DCJdistance(G1,G2))/2
+    # Calculate the Actual distance of the median genome from the other genomes
+    d1=DCJdistance(G1,Child)
+    d2=DCJdistance(G2,Child)
+    d3=DCJdistance(G3,Child)
+    diff1=d1-d1_est
+    diff2=d2-d2_est
+    diff3=d3-d3_est
+    # Mutate the median genome towards the genome it is furthest from
+    if(diff1>=diff2&&diff1>=diff3)
+        mut=DCJ(Child,G1,rand(1:length(G1)))
+    else
+        if(diff2>=diff1&&diff2>=diff3)
+            mut=DCJ(Child,G2,rand(1:length(G2)))
+        else
+            mut=DCJ(Child,G3,rand(1:length(G3)))
+        end
+    end
+    return mut
+end
